0f10de096383706f36a18e023fcf602757af48d5
Kate <82730879+keumul@users.noreply.github.com>
2021-11-23 23:11:41 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,32 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+
+#include "Error.h"
+#include "In.h"
+#include "Log.h"
+#include "Parm.h"
+#include "FST.h"
+#include "IT.h"
+#include "LT.h"
+#include "Lex.h"
+#include "Greibach.h"
+#include "MFST.h"
+#include "PolishNotation.h"
+#include "Graphs.h"
+#include "Generation.h"
+
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cwchar>
+#include <wchar.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <cstdio>
+#include <time.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <queue>
+#include <iomanip>
+#include <stack>
